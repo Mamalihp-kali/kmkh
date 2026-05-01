24423f5b594431dace19b31748384398e66d06bd
Mamalihp-kali <rahimehhosseini599@gmail.com>
2026-05-02 03:06:15 +0330

    Create song.py

---
+++ song.py
@@ -0,0 +1,25 @@
+import time
+import sys
+import random
+
+def sprint(text, base_delay=0.05, variability=0.05)
+  for char in text:
+    sys.stdout.write(char)
+    sys.stdout.flush()
+
+    delay = base_delay + random.uniform(-variability, variability)
+
+    time.sleep(max(0, delay))
+  print()
+print("برای سوالات بیشتر به ایدی تلگرامی @mamdmeti پیام بده")
+#داخل "" متنتو وارد کن و برای تنظیم زمان چاپش واسه base_delay=x.xx بجای x.xx عدد بزاز
+sprint("", base_delay=x.xx, variability=0.03)
+sprint("", base_delay=x.xx, variability=0.03)
+sprint("", base_delay=x.xx, variability=0.03)
+sprint("", base_delay=x.xx, variability=0.03)
+sprint("", base_delay=x.xx, variability=0.03)
+sprint("", base_delay=x.xx, variability=0.03)
+sprint("", base_delay=x.xx, variability=0.03)
+sprint("", base_delay=x.xx, variability=0.03)
+sprint("", base_delay=x.xx, variability=0.03)
+# اگه متنت بیشتر از این بود همین متنای بالارو بزار و اگه کمتر بود پاک کن
